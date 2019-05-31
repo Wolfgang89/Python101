f7affbc56aac42b5147d0fdc27f66ec97f99f4c3
wolfgang <pan.wg@sand.com.cn>
2019-05-31 15:59:49 +0800

    python files update

--- Untitled-1.py
+++ Untitled-1.py
@@ -71,3 +71,83 @@
 # D = {c:c*4 for c in ['a',1,'2']}
 # print(D)
 # 字符串
+# s = 'abcdefg'
+# print(s[0])
+# print(s[-2])
+# print(s[1:4])
+# bd 2 表示步长 隔2最右边不取 1索引隔2 就是3 4 4不取 取1 3 打出来就是bd
+# print(s[1:4:2])
+# bcd
+# print(s[1:4:1])
+#  从-1 到1 最后到1索引 间隔是1 -1是因为是从右边开始算的
+# print(s[-1:1:-1])
+# print(s[-1:1:-3])
+# 字符串的不可修改性
+# S1 = 'abcd'
+# s [0] = '4' 'str' object does not support item assignment
+# S2 = '124'
+# S = S1 + S2
+# print(S)
+# 字符串的赋值 分 深拷贝 和 浅拷贝
+#  深拷贝 即 赋值时 两个对象具有相同的值 但是手机在不用的内存片区的对象 浅拷贝 即 等号左右两边的对象实际上被分配的是同一个内存空间
+# s = ['abcdefg']
+# a = s [:]
+# print(a)
+# print(s is a)
+# s1 = 'abcdefgt'
+# s2 = s1
+# print(s1 == s2)
+# 字符串的遍历 成员测试
+# s = 'hacker'
+# for c in s:
+#     print(c,end= ' ')
+# print('k' in 'momonthor')
+# s = 'ahsisjahaa'
+# print(s.find('ha'))
+# print(s.find('zz'))
+# print(s.replace('ha','XXX'))
+# s = 'Tom,21,USA,UCLA'
+# l = s.split(',')
+# print(l)
+# L = ['s', 'p', 'a', 'm', 'm', 'y']
+# s = '-'.join(L)
+# print(s)
+# s = '   Tom 21 USA UCLA\n\n'
+# print(s)
+# print(s.strip())
+# name = '盘尾'
+# age = 29
+# school = ['CHXY']
+# s = 'name :{},age:{},and graduates form {}'.format(name,age,school)
+# print(s)
+# template = '{1},{0} and {2}'
+# s = template.format('spam','ham','eggs')
+# print(s)
+# template = '{key1},{key2} and {key3}'
+# s = template.format(key1  = 'spam',key2 = 'ham',key3 = 'eggs')
+# print(s)
+# template = 'float number = {:.2f}'
+# s = template.format(10.92625452)
+# print(s)
+# template3 = 'number ={:b}'
+# s = template3.format(2**16 -1)
+# print(s)
+# 字符转义 \t \n 等 和c一样
+# S1 = 's\tname\ne'
+# print(S1)
+# s = r'c:\new\test.py'
+# print(s)
+# s = '19'
+# i = 3
+# print(s+str(i))
+# print(float('1.5'),str(4.56))
+# print(bin(12))
+# print(int('1110',2))
+#       字符编码
+# 计算机自己能理解的语言是二进制数，最小的信息标识是二进制位，8个二进制位表示一个字节，编码就是把人类使用的这些字符集转换为计算机所能理解的二进制码
+# 
+Unicode = '编码方式'
+# 字符编码和字符编码
+
+
+
